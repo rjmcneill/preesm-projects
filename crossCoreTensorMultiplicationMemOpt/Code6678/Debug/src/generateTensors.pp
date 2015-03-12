51bc151050b19cbae72b388e32d55bca452a81af
Robert McNeill <iusedtoselldoors@hotmail.co.uk>
2015-03-12 23:26:03 +0000

    Created project for memory optimised tensor multiplication

---
+++ crossCoreTensorMultiplicationMemOpt/Code6678/Debug/src/generateTensors.pp
@@ -0,0 +1,131 @@
+# FIXED
+
+src/generateTensors.obj: ../src/generateTensors.c
+src/generateTensors.obj: ../src/generateTensors.h
+src/generateTensors.obj: C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include/stdio.h
+src/generateTensors.obj: C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include/linkage.h
+src/generateTensors.obj: C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include/stdarg.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Timestamp.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/std.h
+src/generateTensors.obj: C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include/stdarg.h
+src/generateTensors.obj: C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include/stddef.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/ti/targets/elf/std.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/ti/targets/elf/C66.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/ti/targets/std.h
+src/generateTensors.obj: C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include/stdint.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/xdc.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types__prologue.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/package/package.defs.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types__epilogue.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/ITimestampClient.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IModule.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IHeap.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IInstance.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Memory.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IHeap.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IModule.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Error.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Error__prologue.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Main.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IGateProvider.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IInstance.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IModule.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IModule.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/package/Main_Module_GateProxy.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IInstance.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IGateProvider.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IModule.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Error__epilogue.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/package/Memory_HeapProxy.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IInstance.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IHeap.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IModule.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Error.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Error.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/ITimestampProvider.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/ITimestampClient.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/package/Timestamp_SupportProxy.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/ITimestampProvider.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/package/Timestamp_SupportProxy.h
+src/generateTensors.obj: C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h
+
+../src/generateTensors.c: 
+../src/generateTensors.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include/stdio.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include/stdarg.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Timestamp.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/std.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include/stddef.h: 
+C:/ti/xdctools_3_23_04_60/packages/ti/targets/elf/std.h: 
+C:/ti/xdctools_3_23_04_60/packages/ti/targets/elf/C66.h: 
+C:/ti/xdctools_3_23_04_60/packages/ti/targets/std.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.3.4/include/stdint.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/xdc.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types__prologue.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/package/package.defs.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types__epilogue.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/ITimestampClient.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IHeap.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IInstance.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Memory.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IHeap.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Error.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Error__prologue.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Main.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IGateProvider.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IInstance.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/package/Main_Module_GateProxy.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IInstance.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IGateProvider.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Error__epilogue.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/package/Memory_HeapProxy.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IInstance.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IHeap.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Error.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Error.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/ITimestampProvider.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/ITimestampClient.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/package/Timestamp_SupportProxy.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/ITimestampProvider.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/package/Timestamp_SupportProxy.h: 
+C:/ti/xdctools_3_23_04_60/packages/xdc/runtime/Types.h: 
---
+++ crossCoreTensorMultiplicationMemOpt/Debug/Code6678/Debug/src/subdir.mk
@@ -0,0 +1,21 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+OBJ_SRCS += \
+../Code6678/Debug/src/cache.obj \
+../Code6678/Debug/src/communication.obj \
+../Code6678/Debug/src/displayTensor.obj \
+../Code6678/Debug/src/dump.obj \
+../Code6678/Debug/src/fifo.obj \
+../Code6678/Debug/src/generateTensors.obj \
+../Code6678/Debug/src/main.obj \
+../Code6678/Debug/src/memory.obj \
+../Code6678/Debug/src/multiplyTensors.obj \
+../Code6678/Debug/src/semaphore6678.obj \
+../Code6678/Debug/src/utils.obj 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
